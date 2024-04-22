cbf4eef380676bf3255376a6d7e0f8fdddcc8309
Joseph <joseph@thekelleys.org>
2024-04-22 17:54:18 -0500

    Updated server

--- backend/server.js
+++ backend/server.js
@@ -10,37 +10,8 @@ app.use(express.json());
 // Shows HTTP requests in the console
 app.use(morgan("dev"));
 
-app.get("/hello", (req, res) => {
-    res.send("Hello From the backend");
-})
-
-app.get("/:uname", (req, res) => {
-
-    // sample code for creating a new user
-    // const user = new User({
-    //     uname: req.params.uname,
-    //     pass: "123",
-    //     plants: [],
-    //     tasks: []
-    // })
-
-    // user.save()
-    // .then(function (models) {
-    //     console.log(models);
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //   });
-
-    // User.findOne({uname: {$eq : req.params.uname}})
-    // .then((found) => {
-    //     res.json(found);
-    // })
-    // .catch(function (err) {
-    //   console.log(err);
-    // });
-    
-})
+// Create endpoint http://localhost:8000/api/users
+app.use("/api/users", require("./api/users"));
 
 const port = 8000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
