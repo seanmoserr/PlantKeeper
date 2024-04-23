a30ed4c3134cefc480bbf4751769e71aa6dea018
Joseph <joseph@thekelleys.org>
2024-04-23 08:20:32 -0500

    Added deleteTask functionality

--- frontend/src/plantkeeperApi.js
+++ frontend/src/plantkeeperApi.js
@@ -25,17 +25,20 @@ async function getTasks(uname) {
 
 async function addTasks(uname, taskToAdd) {
     const response = await fetch(`${apiEndpoint}/${uname}`);
+    var taskList;
 
     if (response.ok) {
-        var taskList = await response.json();
+        taskList = await response.json();
         taskList.tasks.push(taskToAdd);
+
         taskList = taskList.tasks;
-        console.log(taskList.tasks);
     } else {
         console.log(response);
         return null;
     }
 
+    console.log(taskList);
+    
     const update = await fetch(`${apiEndpoint}/${uname}`, {
         method: "PUT",
         headers: {
@@ -58,9 +61,54 @@ async function addTasks(uname, taskToAdd) {
 }
 
 // delete task
+async function deleteTask(uname, taskID) {
 
+    // get user object
+    const response = await fetch(`${apiEndpoint}/${uname}`);
+
+    // if ok get task array
+    if (response.ok) {
+        var taskList = await response.json();
+
+        if (taskID === -1){ // if id -1 then remove all tasks
+            taskList = [];
+        } else {
+            // filter tasklist to everything but object with x id
+            taskList = taskList.tasks.filter((obj) => {
+                return obj.id !== taskID;
+            })
+
+        }
+        console.log(taskList);
+    } else {
+        console.log(response);
+        return null;
+    }
+
+    // send update req with updated tasklist
+    const update = await fetch(`${apiEndpoint}/${uname}`, {
+        method: "PUT",
+        headers: {
+           "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            {
+                tasks:taskList
+            }
+        ),
+     })
+
+     if (update.ok) {
+        return taskList;
+     }
+     else {
+        console.log(update);
+        return null;
+    }
+}
 
 export {
     getTasks,
-    addTasks
+    addTasks,
+    deleteTask
 };
