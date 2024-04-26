28552e1ad6aeec8ceab39aeba87669f5f7dc1f20
seanmoserr <seanmoserr@gmail.com>
2024-04-26 11:42:40 -0500

    Revert "Merge branch 'main' of https://github.com/seanmoserr/PlantKeeper"
    This reverts commit 5f4e80c03574a1fce472e669a13d5ca8dee9e01f, reversing
    changes made to e816caa7a9227afb108626e7b01e7e2a4d54d28f.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { getTasks } from './plantkeeperApi';
+import { useState } from 'react';
+import Loginpage from './components/Loginpage';
+import './components/Loginpage.css'
+import { addPlants, deletePlant, getPlants, getTasks, registerUser } from './plantkeeperApi';
+
+const exPlant = {
+  name: "Rosa the Rose",
+  species: "Rose"
+}
+
 
 function App() {
   return (
@@ -10,6 +19,7 @@ function App() {
     <button onClick={(e) => registerUser("eno", "123")}>Register User, Eno</button>
     <button onClick={(e) => registerUser("mr. jones", "321")}>Register User, mr. jones</button>
     </div>
+    </>
   );
 }
 
