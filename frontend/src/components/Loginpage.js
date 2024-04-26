52e347979cf4540d1d3e6f135fa9c37c107b2c88
EnobongOffiong <edoffiong@gmail.com>
2024-04-26 18:24:10 -0500

    Update Loginpage.js

--- frontend/src/components/Loginpage.js
+++ frontend/src/components/Loginpage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+ import { registerUser } from './plantkeeperApi';
 
   // const [count, setCount] = useState(0)
   // function handleClick(){
@@ -20,25 +21,43 @@ import React, { useState } from 'react';
   // }
 function Loginpage(){
 
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
+// const [username, setUsername] = useState('');
+// const [password, setPassword] = useState('');
 
-const [action,setAction] = useState("Login");
+// const [action,setAction] = useState("Login");
 
-function handleSubmit(event){
-    event.preventDefault();
+const [inputs, setInputs] = useState({});
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
+   function handleSubmit(event) {
+      event.preventDefault();
+      alert(`Username: ${inputs.username}\nPassowrd: ${inputs.password}`);
+       
+   }
 
-    if(action==="Login"){
-        console.log("Login form submitted with data: ", data);
-    }
+   function handleChange(event) {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}));
+   }
 
-    if(action==="Sign Up")
 
-    event.target.reset();
-}
+// function handleSubmit(event){
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+//     const data = Object.fromEntries(formData.entries());
+
+//     if(action === "Login"){
+//         console.log({username});
+//     }
+//     else if (action === "Sign Up") {
+//         console.log("Sign Up form submitted with data: ", data);
+//     }
+    
+
+//     setUsername('');
+//     setPassword('');
+// }
 return(
   <>
   
@@ -47,25 +66,30 @@ return(
   <div className='container'>
     
     <div className='header'>
-        <div className='text'>{action}</div>
-        <div className='underline'></div>
+        <div className='text'>Login/Register</div>
+        {/* <div className='underline'></div> */}
     </div>
 
     <form className='inputs' onSubmit={handleSubmit}>
         <div className='input'>
-            <input type='text' id="username" name="username" placeholder='Username'></input>
+            <input type='text' id="username" name="username" placeholder='Username' value={inputs.username || ""}
+               onChange={handleChange}></input>
         </div>
         
         <div className='input'>
-            <input type='password' id="password" name="password" placeholder='Password'></input>
+            <input type='password' id="password" name="password" placeholder='Password' value={inputs.password || ""}
+               onChange={handleChange}></input>
         </div> 
+
+        <div className='submit-container'>
+        <button type="submit" className="submit" >Sign Up</button>
+        <button type="submit" className="submit" >Login</button>
+        
+        </div>
     </form>
     
   
-    <div className='submit-container'>
-        <button type="submit" className="submit" onClick={()=>{setAction("Sign Up")}}>Sign Up</button>
-        <button type="submit"className="submit" onClick={()=>{setAction("Login")}}>Login</button>
-    </div>
+   
   </div>
   </>
     );
