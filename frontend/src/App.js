e816caa7a9227afb108626e7b01e7e2a4d54d28f
seanmoserr <seanmoserr@gmail.com>
2024-04-26 11:38:30 -0500

    Completed check user functionality embedded in registering the user. Logs to console throughout process.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ function App() {
     <>
     <div className='Loginpage'>
     <Loginpage />
+    <button onClick={(e) => registerUser("eno", "123")}>Register User, Eno</button>
+    <button onClick={(e) => registerUser("mr. jones", "321")}>Register User, mr. jones</button>
     </div>
     </>
   );
