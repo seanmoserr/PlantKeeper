5dea4aff0d4cbf9f35a7b4de9f7f32293a8e2b26
Isaiah Shavers <112894709+ishavers@users.noreply.github.com>
2024-04-29 15:12:45 -0500

    home files replaced

---
+++ frontend/src/components/Plant.js
@@ -0,0 +1,10 @@
+
+const Plant = (uname, pname) => {
+    return (
+            <div className="indPlant">
+                <input type="text" placeholder="Enter text..." style="display: block; margin-bottom: 10px;"/>
+                <img src="https://via.placeholder.com/50x50" alt="Small Image" width="50" height="50"/>
+            </div>
+    )
+}
+export default Plant
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,16 +1,22 @@
 import './home.css';
 import { addPlants, deletePlant, getPlants, getTasks, registerUser } from './plantkeeperApi';
+import Plant from "./Plant";
+import {useEffect, useState} from "react";
 
 function loadTasks(uname){
-    const taskList = getTasks(uname);
-    const rows = [];
-    let lastTask = null;
-    /*
-    taskList.forEach() => {
+    const [taskList, setTaskList] = useState([])
 
+    useEffect(() => {
+        getTasks(uname).then((res) => {
+            setTaskList[res]
+        })
+    }, []);
+
+    const renderTaskList = () => {
+        return taskList.forEach((task) => {
+            return <span>{task.getTask()}</span>
+        })
     }
-    */
-    lastTask = rows[taskList.length() - 1];
 
     return (
         <table>
@@ -36,15 +42,16 @@ function plantsPreview(uname) {
     let plant2 = plantList.at(1).getPlantID();
     let plant3 = plantList.at(2).getPlantID();
 
+
     return (
         <div style="border: 2px solid black; padding: 10px; width: 220px; height: 220px;">
             <div style="display: flex; justify-content: space-between;">
-                Plant(uname, plant1)
-                Plant(uname, plant2)
+                <Plant uname={uname} pname={plant1}/>
+                <Plant uname={uname} pname={plant2}/>
             </div>
             <div style="display: flex; justify-content: space-between; margin-top: 10px;">
-                Plant(uname, plant3);
-                <button style="width: 50px; height: 50px;">All Plants</button>
+                <Plant uname={uname} pname={plant3}/>
+                <button onClick = {allPlants(uname)} style="width: 50px; height: 50px;">All Plants</button>
             </div>
         </div>
     );
@@ -52,18 +59,19 @@ function plantsPreview(uname) {
 
 function addTask(uname) {
     return (
-        <button className="task-button">Add Task</button>
+        <button onClick= {addTask(uname)} className="task-button">Add Task</button>
     );
 }
 
 function removeTask(uname, taskID) {
     return (
-        <button className="task-button">Remove Task</button>
+        <button onClick={removeTask(uname, taskID)} className="task-button">Remove Task</button>
     );
 }
 
-function Plant(uname, pname) {
-    return (
+/**
+ function Plant(uname, pname) {
+ return (
         <div className="indPlant">
             <input type="text" placeholder="Enter text..." style="display: block; margin-bottom: 10px;"/>
             <img src="https://via.placeholder.com/50x50" alt="Small Image" width="50" height="50"/>
@@ -71,6 +79,7 @@ function Plant(uname, pname) {
     );
 
 }
+    **/
 
 
 function home(uname) {
@@ -78,6 +87,8 @@ function home(uname) {
     return(
         <div className="home">
             <p>here will output tasks and plants</p>
+            {plantsPreview(uname)}
+            {loadTasks(uname)}
             <div className=".task-button">
                 <p>here will be buttons for the tasks</p>
             </div>
@@ -89,6 +100,8 @@ function allPlants(uname) {
     let plants = [];
     //loop through all of the plants and call plant each time
     return(
+        <>
+        </>
     );
 }
 
